a7d1fe38e37eff3c9ef65ee2c94d2712f595c617
shanmugam <ethirajshanmugam94@gmail.com>
2026-07-27 16:23:54 +0000

    jk flip flop

---
+++ sequential_circuit/jk_ff.v
@@ -0,0 +1,20 @@
+module jk_ff(
+input clk,rst,j,k,
+output reg q);
+always@(posedge clk) begin
+if(rst) 
+q<=1'b0;
+else
+begin
+case({j,k})
+2'b00:q<=q;
+2'b01:q<=1'b0;
+2'b10:q<=1'b1;
+2'b11:q<=~q;
+default:q=1'bx;
+endcase
+end
+end
+endmodule 
+
+
