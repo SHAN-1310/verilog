a57f058c73baad9eb74e490966de391ce2dae505
shanmugam <ethirajshanmugam94@gmail.com>
2026-08-07 05:22:29 +0000

    4 BIT DOWN COUNTER

---
+++ sequential_circuit/downcounter_tb.v
@@ -0,0 +1,25 @@
+module downcounter_tb;
+reg clk;
+reg reset;
+wire[3:0] count;
+downcounter uut(
+.clk(clk),
+.reset(reset),
+.count(count)
+);
+always #5 clk=~clk;
+initial begin
+clk=0;
+reset=1;
+
+#10 reset=0;
+
+//count
+#200;
+$finish;
+end
+initial begin
+$monitor("time=%0t|clk=%b|reset=%b|count=%b(%0d)",$time,clk,reset,count,count);
+end
+endmodule
+
