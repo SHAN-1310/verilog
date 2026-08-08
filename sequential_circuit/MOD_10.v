e7a816358d76c1d7242a8cc0662959fbda59c906
shanmugam <ethirajshanmugam94@gmail.com>
2026-08-08 08:17:14 +0000

    MOD 10 COUNTER

---
+++ sequential_circuit/MOD_10.v
@@ -0,0 +1,13 @@
+module mod_10counter(
+input clk,reset,
+output reg[3:0]count
+);
+always@(posedge clk)begin
+if(reset)
+count<=0;
+else if(count==9)
+count<=0;
+else
+count<=count+1;
+end
+endmodule
